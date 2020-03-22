c095ebecf33790f25cfdad6f098872b387bb97f0
Samed Güner <doktorgibsonyt@gmail.com>
2020-03-22 13:54:00 +0100

    Add subscriptionrequest object

--- fn.go
+++ fn.go
@@ -1,15 +1,37 @@
 package hello
 
 import (
-	"fmt"
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
 )
 
+// SubscriptionRequest ...
+type SubscriptionRequest struct {
+	Email string `json:"email"`
+}
+
 // Subscribe ...
 func Subscribe(w http.ResponseWriter, r *http.Request) {
-	name := r.URL.Query().Get("name")
-	if name == "" {
-		name = "someone"
+	b, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
 	}
-	fmt.Fprintf(w, "Hello, %s!", name)
+
+	var msg SubscriptionRequest
+	err = json.Unmarshal(b, &msg)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	output, err := json.Marshal(msg)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	w.Header().Set("content-type", "application/json")
+	w.Write(output)
 }
