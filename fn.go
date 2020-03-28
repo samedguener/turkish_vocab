1d0bb36f5b619c714a942ef515969ce20de50ef7
Samed Güner <doktorgibsonyt@gmail.com>
2020-03-28 20:30:32 +0100

    Fix subscription test

--- fn.go
+++ fn.go
@@ -3,14 +3,19 @@ package hello
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/mail"
 	"os"
 
 	firebase "firebase.google.com/go"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 )
 
+// Status ...
 type Status struct {
 	Status      string `json:"status"`
 	Description string `json:"description"`
@@ -18,11 +23,8 @@ type Status struct {
 
 // SubscriptionRequest ...
 type SubscriptionRequest struct {
-	Email               string `json:"email"`
-	IsGrammarInterested bool   `json:"grammarInterested"`
-	IsCultureInterested bool   `json:"cultureInterested"`
-	IsHistoryInterested bool   `json:"historyInterested"`
-	IsFoodInterested    bool   `json:"foodInterested"`
+	Email     *string   `json:"email"`
+	Interests []*string `json:"interests"`
 }
 
 // Subscribe ...
@@ -30,6 +32,7 @@ func Subscribe(w http.ResponseWriter, r *http.Request) {
 	b, err := ioutil.ReadAll(r.Body)
 	defer r.Body.Close()
 	if err != nil {
+		log.Printf(err.Error())
 		http.Error(w, err.Error(), 500)
 		return
 	}
@@ -37,21 +40,76 @@ func Subscribe(w http.ResponseWriter, r *http.Request) {
 	var msg SubscriptionRequest
 	err = json.Unmarshal(b, &msg)
 	if err != nil {
+		log.Printf(err.Error())
 		http.Error(w, err.Error(), 500)
 		return
 	}
 
-	subscribed, err := isSubscribed(msg.Email)
+	if msg.Email == nil {
+		err = fmt.Errorf("missing email")
+		log.Printf(err.Error())
+
+		w.Header().Set("content-type", "application/json")
+		status := Status{Status: "failure", Description: "Malformed request! Missing key email!"}
+		output, err := json.Marshal(status)
+		if err != nil {
+			log.Printf(err.Error())
+			http.Error(w, err.Error(), 500)
+			return
+		}
+		w.WriteHeader(400)
+		w.Write(output)
+		return
+	}
+
+	if msg.Interests == nil {
+		err = fmt.Errorf("missing interests")
+		log.Printf(err.Error())
+
+		w.Header().Set("content-type", "application/json")
+		status := Status{Status: "failure", Description: "Malformed request! Missing key interests!"}
+		output, err := json.Marshal(status)
+		if err != nil {
+			log.Printf(err.Error())
+			http.Error(w, err.Error(), 500)
+			return
+		}
+		w.WriteHeader(400)
+		w.Write(output)
+		return
+	}
+
+	_, err = mail.ParseAddress(*msg.Email)
 	if err != nil {
+		log.Printf(err.Error())
+
+		status := Status{Status: "failure", Description: "Malformed request! Malformed email!"}
+		output, errTwo := json.Marshal(status)
+		if errTwo != nil {
+			log.Printf(err.Error())
+			http.Error(w, errTwo.Error(), 500)
+			return
+		}
+
+		w.Header().Set("content-type", "application/json")
+		w.WriteHeader(404)
+		w.Write(output)
+		return
+	}
+
+	subscribed, err := isSubscribed(*msg.Email)
+	if err != nil {
+		log.Printf(err.Error())
 		http.Error(w, err.Error(), 500)
 		return
 	}
 
 	if subscribed {
-		status := Status{Status: "Failed", Description: "Email is already subscribed!"}
+		status := Status{Status: "failure", Description: "Email is already subscribed!"}
 
 		output, err := json.Marshal(status)
 		if err != nil {
+			log.Printf(err.Error())
 			http.Error(w, err.Error(), 500)
 			return
 		}
@@ -61,14 +119,16 @@ func Subscribe(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if err = saveSubscription(msg); err != nil {
+		log.Printf(err.Error())
 		http.Error(w, err.Error(), 500)
 		return
 	}
 
-	status := Status{Status: "Success", Description: "Email is successfully subscribed!"}
+	status := Status{Status: "success", Description: "Email is successfully subscribed!"}
 
 	output, err := json.Marshal(status)
 	if err != nil {
+		log.Printf(err.Error())
 		http.Error(w, err.Error(), 500)
 		return
 	}
@@ -79,10 +139,13 @@ func Subscribe(w http.ResponseWriter, r *http.Request) {
 }
 
 func saveSubscription(data SubscriptionRequest) error {
-	projectID := os.Getenv("GCP_PROJECT")
+	projectID, isSet := os.LookupEnv("GCP_PROJECT")
+	if !isSet {
+		return fmt.Errorf("environment variable 'GCP_PROJECT' not set")
+	}
 
-	ctx := context.Background()
 	conf := &firebase.Config{ProjectID: projectID}
+	ctx := context.Background()
 	app, err := firebase.NewApp(ctx, conf)
 	if err != nil {
 		return err
@@ -94,7 +157,34 @@ func saveSubscription(data SubscriptionRequest) error {
 	}
 	defer client.Close()
 
-	_, err = client.Collection("subscriptions").Doc(data.Email).Set(ctx, data)
+	_, err = client.Collection("subscriptions").Doc(*data.Email).Set(ctx, data)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func deleteSubscription(email string) error {
+	projectID, isSet := os.LookupEnv("GCP_PROJECT")
+	if !isSet {
+		return fmt.Errorf("environment variable 'GCP_PROJECT' not set")
+	}
+
+	conf := &firebase.Config{ProjectID: projectID}
+	ctx := context.Background()
+	app, err := firebase.NewApp(ctx, conf)
+	if err != nil {
+		return err
+	}
+
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+
+	_, err = client.Collection("subscriptions").Doc(email).Delete(ctx)
 	if err != nil {
 		return err
 	}
@@ -103,13 +193,16 @@ func saveSubscription(data SubscriptionRequest) error {
 }
 
 func isSubscribed(email string) (bool, error) {
-	projectID := os.Getenv("GCP_PROJECT")
+	projectID, isSet := os.LookupEnv("GCP_PROJECT")
+	if !isSet {
+		return false, fmt.Errorf("environment variable 'GCP_PROJECT' not set")
+	}
 
 	ctx := context.Background()
 	conf := &firebase.Config{ProjectID: projectID}
 	app, err := firebase.NewApp(ctx, conf)
 	if err != nil {
-		log.Fatalln(err)
+		return false, nil
 	}
 
 	client, err := app.Firestore(ctx)
@@ -118,10 +211,10 @@ func isSubscribed(email string) (bool, error) {
 	}
 	defer client.Close()
 
-	documentRef := client.Collection("subscription").Doc(email)
-	if documentRef != nil {
-		return true, nil
+	_, err = client.Collection("subscriptions").Doc(email).Get(ctx)
+	if grpc.Code(err) == codes.NotFound {
+		return false, nil
 	}
 
-	return false, nil
+	return true, nil
 }
--- fn_test.go
+++ fn_test.go
@@ -1,44 +1,89 @@
 package hello
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
+type Test struct {
+	request            string
+	expectedResponse   string
+	expectedStatusCode int
+	cleanUp            bool
+}
+
 func TestSubscribe(t *testing.T) {
-	tests := map[string]struct {
-		name       string
-		wantStatus int
-		wantString string
-	}{
-		"name specified":     {"jdoe", http.StatusOK, "Hello, jdoe!"},
-		"name not specified": {"", http.StatusOK, "Hello, someone!"},
+
+	tests := []Test{
+		{request: `{ "email" : "malformed", "interests" : [ "food", "grammar", "culture", "vocab"] }`, expectedResponse: `{ "status" : "failure", "description" : "Malformed request! Malformed email!" }`, expectedStatusCode: 404, cleanUp: false},
+		{request: `{ "interests" : [ "food", "grammar", "culture", "vocab"] }`, expectedResponse: `{ "status" : "failure", "description" : "Malformed request! Missing key email!" }`, expectedStatusCode: 400, cleanUp: false},
+		{request: `{ "email" : "mail@samedguener.com" }`, expectedResponse: `{ "status" : "failure", "description" : "Malformed request! Missing key interests!" }`, expectedStatusCode: 400, cleanUp: false},
+		{request: `{ "email" : "mail@samedguener.com", "interests" : [ "food", "grammar", "culture", "vocab"] }`, expectedResponse: `{ "status" : "success", "description" : "Email is successfully subscribed!" }`, expectedStatusCode: 200, cleanUp: false},
+		{request: `{ "email" : "mail@samedguener.com", "interests" : [ "food", "grammar", "culture", "vocab"] }`, expectedResponse: `{ "status" : "failure", "description" : "Email is already subscribed!" }`, expectedStatusCode: 200, cleanUp: true},
 	}
 
-	for name, te := range tests {
-		t.Run(name, func(t *testing.T) {
-			w := httptest.NewRecorder()
-			r := httptest.NewRequest(http.MethodGet, "/", nil)
-			q := r.URL.Query()
-			q.Add("name", te.name)
-			r.URL.RawQuery = q.Encode()
+	for _, test := range tests {
+		req, err := http.NewRequest("POST", "/subscriptions", strings.NewReader(test.request))
+		if err != nil {
+			t.Fatal(err)
+		}
 
-			Subscribe(w, r)
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(Subscribe)
 
-			rw := w.Result()
-			defer rw.Body.Close()
-			if s := rw.StatusCode; s != te.wantStatus {
-				t.Fatalf("got: %d, want: %d", s, te.wantStatus)
-			}
-			b, err := ioutil.ReadAll(rw.Body)
+		handler.ServeHTTP(rr, req)
+
+		if status := rr.Code; status != test.expectedStatusCode {
+			t.Errorf("Status code differs. Expected %d .\n Got %d instead", test.expectedStatusCode, status)
+		}
+
+		var resp Status
+		b, err := ioutil.ReadAll(rr.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		err = json.Unmarshal(b, &resp)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		var expectedResp Status
+		err = json.Unmarshal([]byte(test.expectedResponse), &expectedResp)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if resp.Status != expectedResp.Status {
+			t.Errorf("Status differs. Expected '%s' .\n Got '%s' instead", expectedResp.Status, resp.Status)
+		}
+
+		if resp.Description != expectedResp.Description {
+			t.Errorf("Description differs. Expected '%s' .\n Got '%s' instead", expectedResp.Description, resp.Description)
+		}
+
+		if test.cleanUp {
+			var msg SubscriptionRequest
+
+			b, err := ioutil.ReadAll(strings.NewReader(test.request))
 			if err != nil {
-				t.Fatal("failed to read res body")
+				t.Fatal(err)
 			}
-			if s := string(b); s != te.wantString {
-				t.Fatalf("got: %s, want: %s", s, te.wantString)
+
+			err = json.Unmarshal(b, &msg)
+			if err != nil {
+				t.Fatal(err)
 			}
-		})
+
+			err = deleteSubscription(*msg.Email)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+		}
 	}
+
 }
